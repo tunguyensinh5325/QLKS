289a995720363fe52fa38d1ee0b1776628a06631
dafukgobruh <152757270+dafukgobruh@users.noreply.github.com>
2024-12-21 22:23:26 +0700

    Create ThueBUS.cs

---
+++ QLKSBUS/ThueBUS.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic;
+using QLKSDTO;
+using QLKSDAO;
+
+namespace QLKSBUS
+{
+    public class ThueBUS
+    {
+        public static List<Thue> LayDSThue()
+        {
+            return ThueDAO.LayDSThue();
+        }
+
+        public static bool ThemThue(Thue t)
+        {
+            if (string.IsNullOrEmpty(t.MaPhong) || string.IsNullOrEmpty(t.CMND))
+                return false;
+
+            if (t.NgayDat >= t.NgayTra)
+                return false;
+
+            try
+            {
+                ThueDAO.ThemThue(t);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool XoaThue(string maPhong, string cmnd)
+        {
+            if (string.IsNullOrEmpty(maPhong) || string.IsNullOrEmpty(cmnd))
+                return false;
+
+            try
+            {
+                ThueDAO.XoaThue(maPhong, cmnd);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool CapNhatThue(Thue t)
+        {
+            if (string.IsNullOrEmpty(t.MaPhong) || string.IsNullOrEmpty(t.CMND))
+                return false;
+
+            if (t.NgayDat >= t.NgayTra)
+                return false;
+
+            try
+            {
+                ThueDAO.CapNhatThue(t);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
