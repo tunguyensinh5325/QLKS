033bb44029754cdda8898ec433ff466c8c3b1240
dafukgobruh <152757270+dafukgobruh@users.noreply.github.com>
2024-12-22 11:14:44 +0700

    Update DataProvider.cs

--- QLKSDAO/DataProvider.cs
+++ QLKSDAO/DataProvider.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 using Microsoft.Data.SqlClient;
 using Microsoft.VisualBasic;
 using QLKSDTO;
