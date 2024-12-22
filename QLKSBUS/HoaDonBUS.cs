90788eaab15462c177495b0f6e2b9589503d9f4b
tunguyensinh5325 <caicaicai2005@gmail.com>
2024-12-22 22:40:30 +0700

    Update HoaDonDAO

---
+++ QLKSBUS/HoaDonBUS.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic;
+using QLKSDTO;
+using QLKSDAO;
+
+namespace QLKSBUS
+{
+    public class HoaDonBUS
+    {
+
+        public List<HoaDon> LayDSHoaDon()
+        {
+            return HoaDonDAO.LayDSHoaDon();
+        }
+
+
+        public bool ThemHoaDon(HoaDon hoaDon)
+        {
+
+            if (string.IsNullOrEmpty(hoaDon.MaHD) || string.IsNullOrEmpty(hoaDon.MaPhong) || string.IsNullOrEmpty(hoaDon.CMND))
+            {
+                return false;
+            }
+
+            try
+            {
+                HoaDonDAO.ThemHoaDon(hoaDon);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool XoaHoaDon(string maHD)
+        {
+            if (string.IsNullOrEmpty(maHD))
+            {
+                return false;
+            }
+
+            try
+            {
+                HoaDonDAO.XoaHoaDon(maHD);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool CapNhatHoaDon(HoaDon hoaDon)
+        {
+            if (string.IsNullOrEmpty(hoaDon.MaHD))
+            {
+                return false;
+            }
+
+            try
+            {
+                HoaDonDAO.CapNhatHoaDon(hoaDon);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
